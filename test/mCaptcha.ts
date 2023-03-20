4cac8f86d8c4aec9c7d56c95a9946c6c2ac3b822
Aravinth Manivannan <realaravinth@batsense.net>
2023-03-21 04:25:43 +0530

    feat: copyright notice, access, edit , and simple widget

--- test/mCaptcha.ts
+++ test/mCaptcha.ts
@@ -1,33 +1,21 @@
-import { ExtendDescribeThis } from "nightwatch";
+/*
+ * Copyright (C) 2023  Aravinth Manivannan <realaravinth@batsense.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
-//interface CustomThis {
-//  duckDuckGoUrl: string;
-//  searchBox: string;
-//  submitButton: string;
-//}
-//
-//// callback passed to `describe` should be a regular function (not an arrow function).
-//describe('duckduckgo example', function(this: ExtendDescribeThis<CustomThis>) {
-//  this.duckDuckGoUrl = 'https://duckduckgo.com';
-//  this.searchBox = 'input[name=q]';
-//  this.submitButton = '*[type=submit]';
-//
-//  // callback can be a regular function as well as an arrow function.
-//  beforeEach(function(this: ExtendDescribeThis<CustomThis>, browser) {
-//    browser.navigateTo(this.duckDuckGoUrl!);
-//  });
-//
-//  // no need to specify `this` parameter when passing an arrow function
-//  // as callback to `it`.
-//  it('Search Nightwatch.js and check results', (browser) => {
-//    browser
-//      .waitForElementVisible(this.searchBox!)
-//      .sendKeys(this.searchBox!, ['Nightwatch.js'])
-//      .click(this.submitButton!)
-//      .assert.visible('.results--main')
-//      .assert.textContains('.results--main', 'Nightwatch.js');
-//  });
-//});
+import { ExtendDescribeThis } from "nightwatch";
 
 interface CustomThis {
   mCaptchaUrl: string;
@@ -49,6 +37,14 @@ interface CustomThis {
   peakTraffic: number;
   thresholdTraffic: number;
   simpleSiteKeySubmitButton: string;
+
+  simpleCaptchaAboutUrl: string;
+
+  sitekeyFormWidgetLink: string;
+  sitekeyFormEditWidgetLink: string;
+  sitekeyFormDeleteWidgetLink: string;
+
+  parent_window: string;
 }
 
 // callback passed to `describe` should be a regular function (not an arrow function).
@@ -102,7 +98,7 @@ describe("mCaptcha login example", function (this: ExtendDescribeThis<CustomThis
       .assert.visible(".help-text");
   };
 
-  it("create mCaptcha widget, the simple way", (browser) => {
+  it("create mCaptcha widget, the simple way", async (browser) => {
     //    login(this, browser);
     this.siteKeyDescriptionBox = "input[name=description]";
     this.averageTrafficBox = "input[name=avg_traffic]";
@@ -124,5 +120,109 @@ describe("mCaptcha login example", function (this: ExtendDescribeThis<CustomThis
       .sendKeys(this.thresholdTrafficBox!, [this.thresholdTraffic!.toString()])
       .click(this.simpleSiteKeySubmitButton!)
       .assert.visible(".notification__title-text");
+
+    this.simpleCaptchaAboutUrl = await browser.getCurrentUrl();
+  });
+
+  it("access simple widget and check if all states work", async (browser) => {
+    this.sitekeyFormWidgetLink = ".sitekey-form__widget-link";
+    this.sitekeyFormEditWidgetLink = ".sitekey-form__edit";
+
+    browser
+      .navigateTo(this.simpleCaptchaAboutUrl!)
+      .assert.visible(".notification__title-text");
+
+    //    browser.getAttribute(this.siteKeyDescriptionBox!, "readonly", (v) => {
+    //      expect(v.value).to.be.true;
+    //    });
+
+    this.parent_window = await browser.windowHandle();
+    browser.getAttribute("h1 a", "href", async (link) => {
+      browser.click(this.sitekeyFormWidgetLink!);
+
+      const allHandles = await browser.windowHandles();
+
+      browser
+        .switchWindow(allHandles[1])
+        .assert.urlEquals(
+          `https://demo.mcaptcha.org${link.value?.toString()!}`
+        );
+
+      browser.closeWindow();
+
+      browser.switchWindow(this.parent_window!);
+    });
+  });
+
+  it("access edit simple widget and check if all states work", async (browser) => {
+    browser
+      .navigateTo(this.simpleCaptchaAboutUrl!)
+      .assert.visible(".notification__title-text");
+
+    browser.click(this.sitekeyFormEditWidgetLink!);
+
+    browser.expect.element(this.siteKeyDescriptionBox!).visible;
+    browser.expect.element(this.averageTrafficBox!).visible;
+    browser.expect.element(this.maxTrafficBox!).visible;
+    browser.expect.element(this.thresholdTrafficBox!).visible;
+
+    browser.getAttribute(this.siteKeyDescriptionBox!, "readonly", (v) => {
+      expect(v.value).to.be.null;
+    });
+
+    browser.getAttribute(this.averageTrafficBox!, "readonly", (v) => {
+      expect(v.value).to.be.null;
+    });
+
+    browser.getAttribute(this.maxTrafficBox!, "readonly", (v) => {
+      expect(v.value).to.be.null;
+    });
+
+    browser
+      .click("button.taskbar__add-site")
+      .assert.urlEquals("https://demo.mcaptcha.org/sitekeys/easy/add")
+      .waitForElementVisible(this.averageTrafficBox!)
+      .sendKeys(this.siteKeyDescriptionBox!, [this.siteKeyDescription!])
+      .sendKeys(this.averageTrafficBox!, [
+        (this.averageTraffic! * 2).toString(),
+      ])
+      .sendKeys(this.maxTrafficBox!, [(this.peakTraffic! * 2).toString()])
+      .sendKeys(this.thresholdTrafficBox!, [
+        (this.thresholdTraffic! * 2).toString(),
+      ])
+      .click(this.simpleSiteKeySubmitButton!)
+      .assert.visible(".notification__title-text");
+  });
+
+  it("delete simple widget", async (browser) => {
+    this.sitekeyFormDeleteWidgetLink = ".sitekey-form__delete";
+
+    browser
+      .navigateTo(this.simpleCaptchaAboutUrl!)
+      .assert.visible(".notification__title-text");
+
+    browser.click(this.sitekeyFormDeleteWidgetLink!);
+
+    browser.expect.element(".form__title").text.to.be.equal("Confirm Access");
+
+    browser
+      .waitForElementVisible(this.passwordBox!)
+      .sendKeys(this.passwordBox!, [this.password!]);
+
+    browser.expect.element("#password").attribute("type").to.equal("password");
+    browser.expect.element(this.showPasswordImg!).to.be.visible;
+    browser.expect.element(this.hidePasswordImg!).not.be.visible;
+
+    browser.click(this.showPasswordButton!);
+    browser.expect.element(this.hidePasswordImg!).to.be.visible;
+    browser.expect.element(this.showPasswordImg!).not.be.visible;
+    browser.expect.element("#password").attribute("type").to.equal("text");
+
+    browser.click(this.showPasswordButton!);
+    browser.expect.element("#password").attribute("type").to.equal("password");
+    browser.expect.element(this.showPasswordImg!).to.be.visible;
+    browser.expect.element(this.hidePasswordImg!).not.be.visible;
+
+    browser.click(this.submitButton!).assert.visible(".help-text");
   });
 });
