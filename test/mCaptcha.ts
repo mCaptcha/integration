cdd52545d0c8f8cd1a4bbe6b026b47657c55001c
Aravinth Manivannan <realaravinth@batsense.net>
2023-03-22 06:43:59 +0530

    feat: check secrets component in settings

--- test/mCaptcha.ts
+++ test/mCaptcha.ts
@@ -45,6 +45,9 @@ interface CustomThis {
   sitekeyFormDeleteWidgetLink: string;
 
   parent_window: string;
+
+  settingsCssSelector: string;
+  settingsCopyBtnCssSelector: string;
 }
 
 // callback passed to `describe` should be a regular function (not an arrow function).
@@ -75,6 +78,10 @@ describe("mCaptcha login example", function (this: ExtendDescribeThis<CustomThis
 
   this.sitekeyFormDeleteWidgetLink = ".sitekey-form__delete";
 
+  this.settingsCssSelector =
+    "li.secondary-menu__item:nth-child(3) > a:nth-child(1)";
+  this.settingsCopyBtnCssSelector = ".settings__secret-copy";
+
   // callback can be a regular function as well as an arrow function.
   beforeEach(function (this: ExtendDescribeThis<CustomThis>, browser) {
     browser.navigateTo(this.mCaptchaUrl!);
@@ -104,108 +111,151 @@ describe("mCaptcha login example", function (this: ExtendDescribeThis<CustomThis
 
     browser.click(this.submitButton!).assert.visible(".help-text");
   });
+  //  it("create mCaptcha widget, the simple way", async (browser) => {
+  //    browser
+  //      .click("button.taskbar__add-site")
+  //      .assert.urlEquals("https://demo.mcaptcha.org/sitekeys/easy/add")
+  //      .waitForElementVisible(this.averageTrafficBox!)
+  //      .sendKeys(this.siteKeyDescriptionBox!, [this.siteKeyDescription!])
+  //      .sendKeys(this.averageTrafficBox!, [this.averageTraffic!.toString()])
+  //      .sendKeys(this.maxTrafficBox!, [this.peakTraffic!.toString()])
+  //      .sendKeys(this.thresholdTrafficBox!, [this.thresholdTraffic!.toString()])
+  //      .click(this.simpleSiteKeySubmitButton!)
+  //      .assert.visible(".notification__title-text");
+  //
+  //    this.simpleCaptchaAboutUrl = await browser.getCurrentUrl();
+  //  });
+  //
+  //  it("access simple widget and check if all states work", async (browser) => {
+  //    browser
+  //      .navigateTo(this.simpleCaptchaAboutUrl!)
+  //      .assert.visible(".notification__title-text");
+  //
+  //    //    browser.getAttribute(this.siteKeyDescriptionBox!, "readonly", (v) => {
+  //    //      expect(v.value).to.be.true;
+  //    //    });
+  //
+  //    this.parent_window = await browser.windowHandle();
+  //    browser.getAttribute("h1 a", "href", async (link) => {
+  //      browser.click(this.sitekeyFormWidgetLink!);
+  //
+  //      const allHandles = await browser.windowHandles();
+  //
+  //      browser
+  //        .switchWindow(allHandles[1])
+  //        .assert.urlEquals(
+  //          `https://demo.mcaptcha.org${link.value?.toString()!}`
+  //        );
+  //
+  //      browser.closeWindow();
+  //
+  //      browser.switchWindow(this.parent_window!);
+  //    });
+  //  });
+  //
+  //  it("access edit simple widget and check if all states work", async (browser) => {
+  //    browser
+  //      .navigateTo(this.simpleCaptchaAboutUrl!)
+  //      .assert.visible(".notification__title-text");
+  //
+  //    browser.click(this.sitekeyFormEditWidgetLink!);
+  //
+  //    browser.expect.element(this.siteKeyDescriptionBox!).visible;
+  //    browser.expect.element(this.averageTrafficBox!).visible;
+  //    browser.expect.element(this.maxTrafficBox!).visible;
+  //    browser.expect.element(this.thresholdTrafficBox!).visible;
+  //
+  //    browser.getAttribute(this.siteKeyDescriptionBox!, "readonly", (v) => {
+  //      expect(v.value).to.be.null;
+  //    });
+  //
+  //    browser.getAttribute(this.averageTrafficBox!, "readonly", (v) => {
+  //      expect(v.value).to.be.null;
+  //    });
+  //
+  //    browser.getAttribute(this.maxTrafficBox!, "readonly", (v) => {
+  //      expect(v.value).to.be.null;
+  //    });
+  //
+  //    browser
+  //      .click("button.taskbar__add-site")
+  //      .assert.urlEquals("https://demo.mcaptcha.org/sitekeys/easy/add")
+  //      .waitForElementVisible(this.averageTrafficBox!)
+  //      .sendKeys(this.siteKeyDescriptionBox!, [this.siteKeyDescription!])
+  //      .sendKeys(this.averageTrafficBox!, [
+  //        (this.averageTraffic! * 2).toString(),
+  //      ])
+  //      .sendKeys(this.maxTrafficBox!, [(this.peakTraffic! * 2).toString()])
+  //      .sendKeys(this.thresholdTrafficBox!, [
+  //        (this.thresholdTraffic! * 2).toString(),
+  //      ])
+  //      .click(this.simpleSiteKeySubmitButton!)
+  //      .assert.visible(".notification__title-text");
+  //  });
+  //
+  //  it("delete simple widget", async (browser) => {
+  //    browser
+  //      .navigateTo(this.simpleCaptchaAboutUrl!)
+  //      .assert.visible(".notification__title-text");
+  //
+  //    browser.click(this.sitekeyFormDeleteWidgetLink!);
+  //
+  //    browser.expect.element(".form__title").text.to.be.equal("Confirm Access");
+  //
+  //    browser
+  //      .waitForElementVisible(this.passwordBox!)
+  //      .sendKeys(this.passwordBox!, [this.password!]);
+  //
+  //    browser.expect.element("#password").attribute("type").to.equal("password");
+  //    browser.expect.element(this.showPasswordImg!).to.be.visible;
+  //    browser.expect.element(this.hidePasswordImg!).not.be.visible;
+  //
+  //    browser.click(this.showPasswordButton!);
+  //    browser.expect.element(this.hidePasswordImg!).to.be.visible;
+  //    browser.expect.element(this.showPasswordImg!).not.be.visible;
+  //    browser.expect.element("#password").attribute("type").to.equal("text");
+  //
+  //    browser.click(this.showPasswordButton!);
+  //    browser.expect.element("#password").attribute("type").to.equal("password");
+  //    browser.expect.element(this.showPasswordImg!).to.be.visible;
+  //    browser.expect.element(this.hidePasswordImg!).not.be.visible;
+  //
+  //    browser.click(this.submitButton!).assert.visible(".help-text");
+  //  });
 
-  const login = (obj: ExtendDescribeThis<CustomThis>, browser: any) => {
+  it("Test view and copy secret in settings page", async (browser) => {
+    if (
+      (await browser.getCssProperty(".nav__hamburger-menu", "display")) !=
+      "none"
+    ) {
+      browser.click(".nav__hamburger-menu");
+    }
     browser
-      .waitForElementVisible(obj.usernameBox!)
-      .sendKeys(obj.usernameBox!, [obj.username!])
-      .sendKeys(obj.passwordBox!, [obj.password!])
-      .click(obj.submitButton!)
-      .assert.visible(".help-text");
-  };
+      .click(this.settingsCssSelector!)
+      .waitForElementVisible("#settings__username-form > button:nth-child(2)");
 
-  it("create mCaptcha widget, the simple way", async (browser) => {
+    browser.expect.element("#secret").attribute("type").to.equal("password");
+    browser.expect.element(this.showPasswordImg!).to.be.visible;
+    browser.expect.element(this.hidePasswordImg!).not.be.visible;
+
+    browser.click(this.showPasswordButton!);
+    browser.expect.element(this.hidePasswordImg!).to.be.visible;
+    browser.expect.element(this.showPasswordImg!).not.be.visible;
+    browser.expect.element("#secret").attribute("type").to.equal("text");
+
+    browser.click(this.showPasswordButton!);
+    browser.expect.element("#secret").attribute("type").to.equal("password");
+    browser.expect.element(this.showPasswordImg!).to.be.visible;
+    browser.expect.element(this.hidePasswordImg!).not.be.visible;
+
+    const secret = await browser.getAttribute(
+      this.settingsCopyBtnCssSelector!,
+      "data-secret"
+    );
     browser
-      .click("button.taskbar__add-site")
-      .assert.urlEquals("https://demo.mcaptcha.org/sitekeys/easy/add")
-      .waitForElementVisible(this.averageTrafficBox!)
-      .sendKeys(this.siteKeyDescriptionBox!, [this.siteKeyDescription!])
-      .sendKeys(this.averageTrafficBox!, [this.averageTraffic!.toString()])
-      .sendKeys(this.maxTrafficBox!, [this.peakTraffic!.toString()])
-      .sendKeys(this.thresholdTrafficBox!, [this.thresholdTraffic!.toString()])
-      .click(this.simpleSiteKeySubmitButton!)
-      .assert.visible(".notification__title-text");
-
-    this.simpleCaptchaAboutUrl = await browser.getCurrentUrl();
-  });
-
-  it("access simple widget and check if all states work", async (browser) => {
-    browser
-      .navigateTo(this.simpleCaptchaAboutUrl!)
-      .assert.visible(".notification__title-text");
-
-    //    browser.getAttribute(this.siteKeyDescriptionBox!, "readonly", (v) => {
-    //      expect(v.value).to.be.true;
-    //    });
-
-    this.parent_window = await browser.windowHandle();
-    browser.getAttribute("h1 a", "href", async (link) => {
-      browser.click(this.sitekeyFormWidgetLink!);
-
-      const allHandles = await browser.windowHandles();
-
-      browser
-        .switchWindow(allHandles[1])
-        .assert.urlEquals(
-          `https://demo.mcaptcha.org${link.value?.toString()!}`
-        );
-
-      browser.closeWindow();
-
-      browser.switchWindow(this.parent_window!);
-    });
-  });
-
-  it("access edit simple widget and check if all states work", async (browser) => {
-    browser
-      .navigateTo(this.simpleCaptchaAboutUrl!)
-      .assert.visible(".notification__title-text");
-
-    browser.click(this.sitekeyFormEditWidgetLink!);
-
-    browser.expect.element(this.siteKeyDescriptionBox!).visible;
-    browser.expect.element(this.averageTrafficBox!).visible;
-    browser.expect.element(this.maxTrafficBox!).visible;
-    browser.expect.element(this.thresholdTrafficBox!).visible;
-
-    browser.getAttribute(this.siteKeyDescriptionBox!, "readonly", (v) => {
-      expect(v.value).to.be.null;
-    });
-
-    browser.getAttribute(this.averageTrafficBox!, "readonly", (v) => {
-      expect(v.value).to.be.null;
-    });
-
-    browser.getAttribute(this.maxTrafficBox!, "readonly", (v) => {
-      expect(v.value).to.be.null;
-    });
-
-    browser
-      .click("button.taskbar__add-site")
-      .assert.urlEquals("https://demo.mcaptcha.org/sitekeys/easy/add")
-      .waitForElementVisible(this.averageTrafficBox!)
-      .sendKeys(this.siteKeyDescriptionBox!, [this.siteKeyDescription!])
-      .sendKeys(this.averageTrafficBox!, [
-        (this.averageTraffic! * 2).toString(),
-      ])
-      .sendKeys(this.maxTrafficBox!, [(this.peakTraffic! * 2).toString()])
-      .sendKeys(this.thresholdTrafficBox!, [
-        (this.thresholdTraffic! * 2).toString(),
-      ])
-      .click(this.simpleSiteKeySubmitButton!)
-      .assert.visible(".notification__title-text");
-  });
-
-  it("delete simple widget", async (browser) => {
-    browser
-      .navigateTo(this.simpleCaptchaAboutUrl!)
-      .assert.visible(".notification__title-text");
-
-    browser.click(this.sitekeyFormDeleteWidgetLink!);
-
-    browser.expect.element(".form__title").text.to.be.equal("Confirm Access");
-
-    browser
+      .click("#settings__secret-form > button:nth-child(2)")
+      .pause(1000)
+      .acceptAlert()
       .waitForElementVisible(this.passwordBox!)
       .sendKeys(this.passwordBox!, [this.password!]);
 
@@ -222,7 +272,31 @@ describe("mCaptcha login example", function (this: ExtendDescribeThis<CustomThis
     browser.expect.element("#password").attribute("type").to.equal("password");
     browser.expect.element(this.showPasswordImg!).to.be.visible;
     browser.expect.element(this.hidePasswordImg!).not.be.visible;
+    expect(
+      (await browser
+        .click(this.submitButton!)
+        .waitForElementVisible("#settings__username-form > button:nth-child(2)")
+        .getAttribute(this.settingsCopyBtnCssSelector!, "data-secret")) ===
+        secret
+    ).to.be.false;
+    //  let emailText = await browser
+    //    .click(this.settingsCopyBtnCssSelector!)
+    //    .pause(20000)
+    //    .click("#email")
+    //    .keys([browser.Keys.CONTROL, "a", browser.Keys.BACK_SPACE])
+    //    .keys([browser.Keys.CONTROL, "v"])
+    //    .pause(2000)
+    //    .getText("#email");
+    //  expect(emailText).to.be.equal(secret);
 
-    browser.click(this.submitButton!).assert.visible(".help-text");
+    //  if (
+    //    (await browser.getCssProperty(".nav__hamburger-menu", "display")) !=
+    //    "none"
+    //  ) {
+    //    browser.click(".nav__hamburger-menu");
+    //  }
+    //  browser
+    //    .click(this.settingsCssSelector!)
+    //    .waitForElementVisible("#settings__username-form > button:nth-child(2)");
   });
 });
